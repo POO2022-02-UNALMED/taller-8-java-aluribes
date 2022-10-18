23e9796af5088308400c0d973fe935fd94e9659b
Alejandra Uribe <aluribes@unal.edu.co>
2022-10-18 10:53:03 -0500

    Creando la clase Portero

---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,51 @@
+package futbol;
+
+public class Portero extends Futbolista {
+    public short golesRecibidos;
+    public byte dorsal;
+
+    // Constructor
+
+    public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+        super(nombre, edad, "Portero");
+        this.golesRecibidos = golesRecibidos;
+        this.dorsal = dorsal;
+    }
+
+    // Getters y Setters
+
+    public short getGolesRecibidos() {
+        return golesRecibidos;
+    }
+
+    public void setGolesRecibidos(short golesRecibidos) {
+        this.golesRecibidos = golesRecibidos;
+    }
+
+    public byte getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(byte dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    // Métodos
+
+    @Override
+    public String toString() {
+        return "El futbolista " + getNombre() + " tiene " + getEdad() + ", y juega de " + getPosicion()+ " con el dorsal "+ dorsal+ ". Le han marcado "+ golesRecibidos ;
+    }
+
+
+    @Override
+    public boolean jugarConLasManos() {
+        return true;
+    }
+
+    @Override
+    public int compareTo(Object obj){
+        return Math.abs(this.golesRecibidos-((Portero)obj).golesRecibidos);
+    }
+
+}
